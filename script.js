a4a12d068e9133646d111fc1ee5935a3a2d19464
MK <mk@opinion.com.pl>
2017-11-27 18:58:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let a=h=triangleArea = 0.0;
+
+a = prompt("a:");
+h = prompt("h:");
+
+triangleArea = a/2*h;
+
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
